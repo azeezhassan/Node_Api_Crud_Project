86fe1e6e55b58d330261aa9ed68f8100feba0405
azeezhassan <bolfas2009@gmail.com>
2023-10-08 02:00:57 +0100

    create a product model

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 //Route
 app.get('/',(req,res) => {
     res.send('Hello NODE API')
 })
 
-// listen port
-app.listen(3000,()=>{
-    console.log('Node API App is running on port 3000')
+app.get('/blog',(req,res) => {
+    res.send('Hello Blog My name is Hassan')
+})
+
+//Mongoose Db Connection before app listening
+mongoose.connect('mongodb://localhost:27017/NodeApis')
+.then(() => {
+    console.log('Connected to MongoDb')
+    // listen port
+    app.listen(3000,()=>{
+        console.log('Node API App is running on port 3000')
+    })
+    
+}).catch((error) =>{
+    console.log(error)
 })
